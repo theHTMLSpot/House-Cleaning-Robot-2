1ee210da2bcead5123c63f38e9a2cf7c5d325e38
theHTMLSpot <121297730+theHTMLSpot@users.noreply.github.com>
2024-11-14 19:06:32 +0100

    Update sketch.ino

--- Code/ESP32-Main/sketch.ino
+++ Code/ESP32-Main/sketch.ino
@@ -1,7 +1,7 @@
-#include <servo.h>
+#include <Servo.h>
 
-#define echo 1
-#define trig 0
+#define echoPin 1
+#define trigPin 0
 #define stepR 7
 #define dirR 6
 #define stepL 8
@@ -9,7 +9,7 @@
 #define kneeR 4
 #define kneeL 5
 
-#define stepsPerRevolution 200;
+#define stepsPerRevolution 200
 
 Servo Left;
 Servo Right;
@@ -20,8 +20,8 @@ void setup() {
   Left.attach(kneeL);
   Right.attach(kneeR);
 
-  pinMode(echo, INPUT);
-  pinMode(trig, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(trigPin, OUTPUT);
   pinMode(stepR, OUTPUT);
   pinMode(dirR, OUTPUT);
   pinMode(stepL, OUTPUT);
@@ -32,66 +32,62 @@ void setup() {
 
 void loop() {
   long duration, distance;
-  digitalWrite(trig, LOW);
+
+  // Send ultrasonic pulse
+  digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  digitalWrite(trig, HIGH);
+  digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
-  digitalWrite(LOW);
-    
-  duration = pulseIn(echo, HIGH);
-  distance = (duration/2) / 29.1;
+  digitalWrite(trigPin, LOW);
+
+  // Calculate distance
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
   Serial.println(distance);
 
-  digitalWrite(stepL, HIGH);
-  digitalWrite(stepR, HIGH);
-
-  delay(500);
-  if(distance < 20)
-  {
+  // Trigger movement if object is within 20 cm
+  if (distance < 20) {
     Left.write(90);
-    right.write(90)
+    Right.write(90);
     delay(500);
-    if ( distance !< 20)
-    {
-      return;
-    }
+
+    if (distance >= 20) return;
+
     Left.write(45);
     Right.write(45);
     delay(500);
-     if ( distance !< 20)
-    {
-      return;
-    }
+
+    if (distance >= 20) return;
+
     Left.write(180);
     Right.write(180);
 
-    digitalWrite(dirL, LOW);  
+    // Set motor directions
+    digitalWrite(dirL, LOW);
     digitalWrite(dirR, LOW);
 
     digitalWrite(stepL, HIGH);
     digitalWrite(stepR, HIGH);
-
     delay(5000);
 
-    digitalWrite(stepL, LOW );
+    digitalWrite(stepL, LOW);
     digitalWrite(stepR, LOW);
 
+    // Change direction for turning
     digitalWrite(dirL, HIGH);
-    digitalWrite(dirR, LOW); 
-
+    digitalWrite(dirR, LOW);
 
     digitalWrite(stepL, HIGH);
     digitalWrite(stepR, HIGH);
-
     delay(500);
 
-    digitalWrite(stepL, LOW );
+    digitalWrite(stepL, LOW);
     digitalWrite(stepR, LOW);
 
-    digitalWrite(dirL, HIGH);  
+    // Reset both directions forward
+    digitalWrite(dirL, HIGH);
     digitalWrite(dirR, HIGH);
-
   }
 
-  
- }
+  delay(100);
+}
